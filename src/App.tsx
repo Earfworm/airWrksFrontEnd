025d2fdfa07f6283c58e8cbff35bd1f8058b1876
earfworm <deshawnreid2021@gmail.com>
2023-02-22 03:07:13 -0500

    no script needs to be solved

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import About from "./components/About";
+import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </div>
