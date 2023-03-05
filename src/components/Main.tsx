25b9d5044c14f099f0d8a4bbac619e65c008e5ee
earfworm <deshawnreid2021@gmail.com>
2023-03-05 17:02:13 -0500

    p5.js react install now i gotta figure out how to implement it

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,21 @@
 import "./Main.css";
+import Sketch from "react-p5";
 
 const Main = () => {
-  return <div className="Main">Main works</div>;
+  // let x = 50;
+  // let y = 50;
+  // const setup = (p5, parent) => {
+  //   p5.createCanvass(700, 500).parent(parent);
+  //   p5.background(0);
+  // };
+
+  // const draw = (p5) => {
+  //   p5.ellipse(x, y, 70, 70);
+  //   x++;
+  // };
+  return (
+    <div className="Main">{/* <Sketch setup={setup} draw={draw} /> */}</div>
+  );
 };
 
 export default Main;
