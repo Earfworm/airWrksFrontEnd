9941ba565711fb2f6abd3c810b32a5ec84979f75
earfworm <deshawnreid2021@gmail.com>
2023-03-06 01:04:56 -0500

    imported p5.js

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,21 +1,25 @@
 import "./Main.css";
 import Sketch from "react-p5";
+import p5Types from "p5";
 
+interface ComponentProps {
+  //Your component props
+}
 const Main = () => {
-  // let x = 50;
-  // let y = 50;
-  // const setup = (p5, parent) => {
-  //   p5.createCanvass(700, 500).parent(parent);
-  //   p5.background(0);
-  // };
+  let x = 50;
+  const y = 50;
 
-  // const draw = (p5) => {
-  //   p5.ellipse(x, y, 70, 70);
-  //   x++;
-  // };
-  return (
-    <div className="Main">{/* <Sketch setup={setup} draw={draw} /> */}</div>
-  );
+  //See annotations in JS for more information
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(100, 100).parent(canvasParentRef);
+  };
+
+  const draw = (p5: p5Types) => {
+    p5.background(0);
+    p5.ellipse(x, y, 70, 70);
+    x++;
+  };
+  return <div className="Main">{<Sketch setup={setup} draw={draw} />}</div>;
 };
 
 export default Main;
