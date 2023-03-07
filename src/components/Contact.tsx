7d16a22f4f4312a342c135894260080c890412f7
earfworm <deshawnreid2021@gmail.com>
2023-03-06 19:58:25 -0500

    some more form work didn't realize their is an error: You provided a  prop to a form field without an  handler. This will render a read-only field. If the field should be mutable use . Otherwise, set either  or . gotta fix that tomorrow

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,16 +4,16 @@ const Contact = () => {
   return (
     <form className="Contact">
       <label className="name">Name</label>
-      <input type="text" id="name" name="name" value={""} />
+      <input type="text" id="name" name="name" value="" />
       <label className="contact">Contact Info</label>
       <input
         type="text"
         id="contact"
         name="contact"
-        value={"How would like to be contacted?"}
+        value="How would like to be contacted?"
       />
-      <label className="message">Message</label>
-      <input type="text" id="message" name="messaget" value={""} />
+      <label className="message">Leave a Message</label>
+      <input type="text" id="message" name="message" value="" />
     </form>
   );
 };
