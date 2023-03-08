9c5c92379d2b086c5e8350ee95ca5ab630c4c036
earfworm <deshawnreid2021@gmail.com>
2023-03-07 19:36:45 -0500

    quick onChange add that error code disappeared gotta set up state varibles and props for the form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,16 +4,17 @@ const Contact = () => {
   return (
     <form className="Contact">
       <label className="name">Name</label>
-      <input type="text" id="name" name="name" value="" />
+      <input type="text" id="name" name="name" value="" onChange={} />
       <label className="contact">Contact Info</label>
       <input
         type="text"
         id="contact"
         name="contact"
         value="How would like to be contacted?"
+        onChange={}
       />
       <label className="message">Leave a Message</label>
-      <input type="text" id="message" name="message" value="" />
+      <input type="text" id="message" name="message" value="" onChange={} />
     </form>
   );
 };
