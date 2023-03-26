c06ade2fe0b01cb197848aa9e9e5e5b7aca91038
earfworm <deshawnreid2021@gmail.com>
2023-03-26 15:24:49 -0400

    the code was fine

--- src/services/ContactAirWrksService.ts
+++ src/services/ContactAirWrksService.ts
@@ -6,8 +6,9 @@ const baseUrl: string = process.env.REACT_APP_API_URL || "";
 export const addNewContact = (
   newContact: ContactAirWrks
 ): Promise<ContactAirWrks> => {
+  console.log(newContact);
   return axios
-    .post(`${baseUrl}/contactAirWrks`, newContact)
+    .post(`http://localhost:4000/contactAirWrks`, newContact)
     .then((res) => res.data)
     .catch((error) => {
       console.log(error);
