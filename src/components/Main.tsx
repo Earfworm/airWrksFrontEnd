98e18ae1b037c53b8c2c31ef5690bd26ec4970a7
earfworm <deshawnreid2021@gmail.com>
2023-04-01 15:54:06 -0400

    header fixed top

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,20 +6,25 @@ interface ComponentProps {
   //Your component props
 }
 const Main = () => {
-  let x = 50;
-  const y = 50;
+  // let x = 50;
+  // const y = 50;
 
-  //See annotations in JS for more information
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
-    p5.createCanvas(100, 100).parent(canvasParentRef);
-  };
+  // //See annotations in JS for more information
+  // const setup = (p5: p5Types, canvasParentRef: Element) => {
+  //   p5.createCanvas(100, 100).parent(canvasParentRef);
+  // };
 
-  const draw = (p5: p5Types) => {
-    p5.background(0);
-    p5.ellipse(x, y, 70, 70);
-    x++;
-  };
-  return <div className="Main">{<Sketch setup={setup} draw={draw} />}</div>;
+  // const draw = (p5: p5Types) => {
+  //   p5.background(0);
+  //   p5.ellipse(x, y, 70, 70);
+  //   x++;
+  // };
+  return (
+    <div className="Main">
+      {/* {<Sketch setup={setup} draw={draw} />} */}
+      <p>main page</p>
+    </div>
+  );
 };
 
 export default Main;
