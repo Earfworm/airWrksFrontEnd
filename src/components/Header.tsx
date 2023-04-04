a7856982102be89ed0c053e523363aefea853c87
earfworm <deshawnreid2021@gmail.com>
2023-04-03 21:44:20 -0400

    hamburger menu for mobile created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,50 @@
 import { Link } from "react-router-dom";
 
 import "./Header.css";
+import { Component } from "react";
 
-const Header = () => {
-  return (
-    <div className="Header">
-      <Link to={"/"}>
-        <h1 className="header-logo">airWrks</h1>
-      </Link>
-      <nav className="header-nav">
-        <ul>
-          <Link to={"/about"}>
-            <div className="play">
-              <p className="about-p">About</p>
-            </div>
-          </Link>
-          <a href="https://www.linkedin.com/in/deshawnreid/">
-            <div className="stop">
-              <p className="social-p">Social</p>
-            </div>
-          </a>
-          <Link to={"/contact"}>
-            <div className="record">
-              <p className="contact-p">Contact</p>
-            </div>
-          </Link>
-        </ul>
-      </nav>
-    </div>
-  );
-};
+class Header extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <div className="Header">
+        <Link to={"/"}>
+          <h1 className="header-logo">airWrks</h1>
+        </Link>
+        <nav className="header-nav">
+          <ul
+            id="navbar"
+            className={this.state.clicked ? "navbar active" : "navbar"}
+          >
+            <Link to={"/about"}>
+              <div className="play">
+                <p className="about-p">About</p>
+              </div>
+            </Link>
+            <a href="https://www.linkedin.com/in/deshawnreid/">
+              <div className="stop">
+                <p className="social-p">Social</p>
+              </div>
+            </a>
+            <Link to={"/contact"}>
+              <div className="record">
+                <p className="contact-p">Contact</p>
+              </div>
+            </Link>
+          </ul>
+          <div id="mobile" onClick={this.handleClick}>
+            <i
+              id="bar"
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
 
 export default Header;
