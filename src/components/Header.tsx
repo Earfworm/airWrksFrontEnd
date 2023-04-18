b07fa0f68075af5103bd7fc37e4bc039c7ff612e
earfworm <deshawnreid2021@gmail.com>
2023-04-17 21:38:01 -0400

    hamburger menu removes when link is click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,14 @@ class Header extends Component {
   handleClick = () => {
     this.setState({ clicked: !this.state.clicked });
   };
+  handleMenuClick = () => {
+    this.setState({ clicked: false });
+  };
+
   render() {
     return (
       <div className="Header">
-        <Link to={"/"}>
+        <Link to={"/"} onClick={this.handleMenuClick}>
           <h1 className="header-logo">airWrks</h1>
         </Link>
         <nav className="header-nav">
@@ -19,17 +23,20 @@ class Header extends Component {
             id="navbar"
             className={this.state.clicked ? "navbar active" : "navbar"}
           >
-            <Link to={"/about"}>
+            <Link to={"/about"} onClick={this.handleMenuClick}>
               <div className="play">
                 <p className="about-p">About</p>
               </div>
             </Link>
-            <a href="https://www.linkedin.com/in/deshawnreid/">
+            <a
+              href="https://www.linkedin.com/in/deshawnreid/"
+              onClick={this.handleMenuClick}
+            >
               <div className="stop">
                 <p className="social-p">Social</p>
               </div>
             </a>
-            <Link to={"/contact"}>
+            <Link to={"/contact"} onClick={this.handleMenuClick}>
               <div className="record">
                 <p className="contact-p">Contact</p>
               </div>
