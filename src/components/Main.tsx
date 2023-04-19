3508e34b2eb6a230bad1b191eb7a2679aa6cb5c4
earfworm <deshawnreid2021@gmail.com>
2023-04-18 23:54:13 -0400

    main page slogan basic keyframes in

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,27 +2,16 @@ import "./Main.css";
 import Sketch from "react-p5";
 import p5Types from "p5";
 
-interface ComponentProps {
-  //Your component props
-}
+interface ComponentProps {}
 const Main = () => {
-  // let x = 50;
-  // const y = 50;
-
-  // //See annotations in JS for more information
-  // const setup = (p5: p5Types, canvasParentRef: Element) => {
-  //   p5.createCanvas(100, 100).parent(canvasParentRef);
-  // };
-
-  // const draw = (p5: p5Types) => {
-  //   p5.background(0);
-  //   p5.ellipse(x, y, 70, 70);
-  //   x++;
-  // };
   return (
     <div className="Main">
-      {/* {<Sketch setup={setup} draw={draw} />} */}
-      <p>main page</p>
+      <p>
+        <span id="beats-span">beats</span>
+        <span id="rhymes-span">Rhymes</span>
+        <span id="code-span">Code</span>
+        <span id="life-span">Life</span>
+      </p>
     </div>
   );
 };
