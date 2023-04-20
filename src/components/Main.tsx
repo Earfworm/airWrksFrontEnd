7548cc0027e9203fd6867d16b8868a43a3778e0a
earfworm <deshawnreid2021@gmail.com>
2023-04-20 14:53:28 -0400

    beatmachine css design 2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,30 +14,36 @@ const Main = () => {
           <div id="info-box">
             <h3>Music Production Workshop</h3>
           </div>
-          <div className=" row-1">
+          <div className="rows row-1">
             <div className="volume"></div>
             <div className="pan"></div>
-            <div className="step"></div>
+            <div className="channel">
+              <p id="kick">Kick</p>
+            </div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
           </div>
-          <div className="row-2">
+          <div className="rows row-2">
             <div className="volume"></div>
             <div className="pan"></div>
-            <div className="step"></div>
+            <div className="channel">
+              <p id="snare">Snare</p>
+            </div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
           </div>
-          <div className="row-3">
+          <div className="rows row-3">
             <div className="volume"></div>
             <div className="pan"></div>
-            <div className="step"></div>
+            <div className="channel">
+              <p id="hi-hats">Hi-Hats</p>
+            </div>
             <div className="step"></div>
             <div className="step"></div>
             <div className="step"></div>
