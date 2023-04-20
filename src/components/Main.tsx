a4fd31b3e9e6e0b47e40fea8853fc2a9f6d3531b
earfworm <deshawnreid2021@gmail.com>
2023-04-20 01:47:12 -0400

    main page beatmachine design 1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,6 +9,43 @@ const Main = () => {
         <span id="code-span">Code</span>
         <span id="life-span">Life</span>
       </p>
+      <div className="beatMachine outerShell">
+        <div className="innerShell">
+          <div id="info-box">
+            <h3>Music Production Workshop</h3>
+          </div>
+          <div className=" row-1">
+            <div className="volume"></div>
+            <div className="pan"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+          </div>
+          <div className="row-2">
+            <div className="volume"></div>
+            <div className="pan"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+          </div>
+          <div className="row-3">
+            <div className="volume"></div>
+            <div className="pan"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+            <div className="step"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
