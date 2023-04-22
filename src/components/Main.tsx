34ee2ec3f5aaa803e882c52ab79ea55a6d2c838e
earfworm <deshawnreid2021@gmail.com>
2023-04-21 21:40:20 -0400

    main page update

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,52 +3,56 @@ import "./Main.css";
 const Main = () => {
   return (
     <div className="Main">
-      <p id="beatsRhymeCodeLife">
-        <span id="beats-span">beats</span>
-        <span id="rhymes-span">Rhymes</span>
-        <span id="code-span">Code</span>
-        <span id="life-span">Life</span>
-      </p>
-      <div className="beatMachine outerShell">
-        <div className="innerShell">
-          <div id="info-box">
-            <h3>Music Production Workshop</h3>
-          </div>
-          <div className="rows row-1">
-            <div className="volume"></div>
-            <div className="pan"></div>
-            <div className="channel">
-              <p id="kick">Kick</p>
+      <div id="beatsRhymesCodeLife-container">
+        <p id="beatsRhymeCodeLife">
+          <span id="beats-span">beats</span>
+          <span id="rhymes-span">Rhymes</span>
+          <span id="code-span">Code</span>
+          <span id="life-span">Life</span>
+        </p>
+      </div>
+      <div id="beatMachine-Container">
+        <div className="beatMachine outerShell">
+          <div className="innerShell">
+            <div id="info-box">
+              <h3>Music Production Workshop</h3>
             </div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-          </div>
-          <div className="rows row-2">
-            <div className="volume"></div>
-            <div className="pan"></div>
-            <div className="channel">
-              <p id="snare">Snare</p>
+            <div className="rows row-1">
+              <div className="volume"></div>
+              <div className="pan"></div>
+              <div className="channel">
+                <p id="kick">Kick</p>
+              </div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
             </div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-          </div>
-          <div className="rows row-3">
-            <div className="volume"></div>
-            <div className="pan"></div>
-            <div className="channel">
-              <p id="hi-hats">Hi-Hats</p>
+            <div className="rows row-2">
+              <div className="volume"></div>
+              <div className="pan"></div>
+              <div className="channel">
+                <p id="snare">Snare</p>
+              </div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
             </div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
-            <div className="step"></div>
+            <div className="rows row-3">
+              <div className="volume"></div>
+              <div className="pan"></div>
+              <div className="channel">
+                <p id="hi-hats">Hi-Hats</p>
+              </div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+              <div className="step"></div>
+            </div>
           </div>
         </div>
       </div>
