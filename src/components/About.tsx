4cb9c4121e0dd12fd1b8b634d49749e51d0918b3
earfworm <deshawnreid2021@gmail.com>
2023-05-25 21:36:13 -0400

    started putting content on about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,34 @@
 import "./About.css";
+import RotatingCircle from "./RotatingCircle";
 
 const About = () => {
   return (
     <div className="About">
-      <p>
-        airWrks is a music production workshop that integrates technology and
-        creativity to help the youth develop their music production skills.
-        Through the use of FL Studio, participants learn how to record, edit,
-        mix, and master audio, and gain insight into the similarities between
-        coding and music production. By exploring the interplay between
-        technology and creativity, airWrks empowers individuals to create music
-        that is both technically sophisticated and artistically expressive.
-      </p>
+      <div className="about-container">
+        <RotatingCircle />
+        <p>
+          airWrks is a music production workshop that integrates technology and
+          creativity to help the youth develop their music production skills.
+          Through the use of FL Studio, participants learn how to record, edit,
+          mix, and master audio, and gain insight into the similarities between
+          coding and music production. By exploring the interplay between
+          technology and creativity, airWrks empowers individuals to create
+          music that is both technically sophisticated and artistically
+          expressive.
+        </p>
+      </div>
+      <div className="goals-container">
+        <h3>Develop Technical Skills</h3>
+        <img src="image.png" alt="a wrench" />
+        <h3>Encourage Creativity</h3>
+        <img src="image.png" alt="a wrench" />
+        <h3>Develop Critical Listening Skills</h3>
+        <img src="image.png" alt="a wrench" />
+        <h3>Encourage Collaboration</h3>
+        <img src="image.png" alt="a wrench" />
+        <h3>Foster A Love Of Music</h3>
+        <img src="image.png" alt="a wrench" />
+      </div>
     </div>
   );
 };
